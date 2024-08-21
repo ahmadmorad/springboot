5b93d9072054afa3c62e5b1cf48e6e42cd2e36a1
AhmadsLaptop <ahmadmoradhr@gmail.com>
2024-08-21 19:45:08 +0300

    uploading the project file

---
+++ raptor/src/main/java/dev/ahmad/raptor/RaptorApplication.java
@@ -0,0 +1,13 @@
+package dev.ahmad.raptor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RaptorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RaptorApplication.class, args);
+	}
+
+}
---
+++ raptor/src/test/java/dev/ahmad/raptor/RaptorApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.ahmad.raptor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RaptorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
